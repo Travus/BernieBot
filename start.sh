ae8d3cc91af0c05ec3008d90fb21f3bcc7a829f8
Travus <realtravus@outlook.com>
2020-10-25 23:53:50 +0100

    Updates start script and compose files.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp /usr/src/app/temp_modules/*.py /usr/src/app/modules/
+find ./temp_modules/ -type f -name "*.py" -exec cp -t ./modules/ {} +
 rm -rf /usr/src/app/temp_modules
 python /usr/src/app/main.py

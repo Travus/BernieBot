29827468986fcfc7400ad61ce0b5e3faa7f089fd
Travus <realtravus@outlook.com>
2020-11-22 02:17:24 +0100

    (utils) Added utils module with empty utils cog.

---
+++ modules/utils.py
@@ -0,0 +1,35 @@
+from discord.ext import commands  # For implementation of bot commands.
+
+import travus_bot_base as tbb  # TBB functions and classes.
+
+
+def setup(bot: tbb.TravusBotBase):
+    """Setup function ran when module is loaded."""
+    bot.add_cog(UtilsCog(bot))  # Add cog and command help info.
+    bot.add_module("Utils", "[Travus](https://github.com/Travus):\n\tCommands", UtilsCog.usage,
+                   """This module includes various utility commands, such as setting server info, user count, setting
+                   reminders, etc. There commands are meant for use by both regular users and moderators and the
+                   commands are intended to provide some value, opposed to just give fun responses.""")
+
+
+def teardown(bot: tbb.TravusBotBase):
+    """Teardown function ran when module is unloaded."""
+    bot.remove_cog("UtilsCog")  # Remove cog and command help info.
+    bot.remove_module("Utils")
+    bot.remove_command_help(UtilsCog)
+
+
+class UtilsCog(commands.Cog):
+    """Cog that holds utils functionality."""
+
+    def __init__(self, bot: tbb.TravusBotBase):
+        """Initialization function loading bot object for cog."""
+        self.bot = bot
+
+    @staticmethod
+    def usage() -> str:
+        """Returns the usage text."""
+        return ("**How To Use The Utils Module:**\nThis module holds miscellaneous utility commands. It has features "
+                "such as getting information about the server, the amount of users in the server, setting reminders "
+                "and more. For information on how to use the commands in this module, check their respective help "
+                "entries.")
